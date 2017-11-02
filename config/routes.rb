a8e74cc2555b8f652234e92a0d94357a2a5de1ba
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-02 16:14:48 -0700

    converted rails applicatino to RESFUL

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homepages#home'
 
+  namespace :api do 
+  	namespace :v1 do
+  		resources :tasks
+  	end
+  end
+
   
 end

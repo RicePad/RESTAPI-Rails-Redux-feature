fb0a03b5a84e89f09a8ca4f6ce1492c2a3ee73a2
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-02 15:56:08 -0700

    React setup

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+
+const appComponent = document.getElementById('main-app')
+ReactDOM.render(
+	<App />, appComponent
+
+	)

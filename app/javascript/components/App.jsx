8bc40a4b0cd074a7643c4bea4735a62f40ebb514
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-02 16:19:31 -0700

    Set up Axios to make HTTPS request

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			tasks: []
+		}
+	}
+	
+	componentDidMount() {
+		axios.get('http://localhost:5000/api/v1/tasks.json')
+			.then(response => {
+				console.log(response)
+				this.setState({tasks: response.date})
+
+			})
+			.catch(error => console.log(error))
+	}
+
 	render(){
 		return(
 			<div>

aa1e4664bbc8f34f3562951ba2d1923822a6f4d7
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-02 20:25:58 -0700

    integrated post request

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TasksController < ApplicationController
+	skip_before_action :verify_authenticity_token
+
 
 	def index
 		@tasks = Task.all
@@ -9,5 +11,17 @@ class Api::V1::TasksController < ApplicationController
 		@task = Task.find(params[:id])
 		render json: @task
 	end
+
+	def create 
+		@task = Task.create(task_params)
+		render json: @task
+
+	end
+
+	private
+
+		def task_params 
+      		params.require(:task).permit(:title, :body)
+		end
 end
  
--- app/javascript/components/TasksList.jsx
+++ app/javascript/components/TasksList.jsx
@@ -2,30 +2,58 @@ import React, { Component } from 'react';
 import TaskItem from './TaskItem';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import update from 'immutability-helper';
+
 
 
 
 class TasksList extends Component {
+		constructor(props){
+			super(props);
+
+			this.state = {
+				tasks: []
+			}
+		}
 	
 
-		// componentDidMount() {
-		// 	axios.get('http://localhost:5000/api/v1/tasks.json')
-		// 		.then(response => {
-		// 			// console.log(response)
-		// 			this.props.showTasks()
-		// 		})
+		componentDidMount() {
+			axios.get('http://localhost:5000/api/v1/tasks.json')
+				.then(response => {
+					// console.log(response)
+					this.setState({tasks: response.data})
+				})
 
-		// 		.catch(error => console.log(error))
-		// }
+				.catch(error => console.log(error))
+		}
+
+
+		addTask = () => {
+		    axios.post('http://localhost:5000/api/v1/tasks', {task: {title: '', body: ''}})
+		    .then(response => {
+		     	const tasks = update(this.state.tasks, { $splice: [[0, 0, response.data]]})
+		        console.log(response)
+
+		        this.setState({tasks: tasks})
+		    })
+		    .catch(error => console.log(error))
+  }
 
 		render(){
 			// console.log('this.props-xx', this.props)
 			return(
 				<div>
 					<div>
+						<button 
+							className="addTaskButton"
+							onClick={this.addTask}
+						> Add Task
+						</button>
+					</div>
+					<div>
 						
 						{
-							this.props.fetchTasks.map((task, index) => {
+							this.state.tasks.map((task, index) => {
 								return(
 									<TaskItem 
 										task={task} 

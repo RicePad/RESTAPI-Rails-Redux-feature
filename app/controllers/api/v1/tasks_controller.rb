4e68dd6d18fcdf3f573f2b368f2b80409bcbe151
JON <jonn.thinkrtc@gmail.com>
2018-01-05 13:18:29 -0800

    updated all components with react

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -18,6 +18,16 @@ class Api::V1::TasksController < ApplicationController
 
 	end
 
+	def destroy
+		@task = Task.find(params[:id])
+		if @task.destroy
+			head :no_content, status: :ok
+		else
+			render json: @task.errors, status: :unprocessable_entity
+		end
+	end
+
+
 	def update
 		@task = Task.find(params[:id])
 		@task.update_attributes(task_params)

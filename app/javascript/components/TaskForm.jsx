9586c1e3fe6c2a5e97e5ff0f442728f739fb4054
JON <jonn.thinkrtc@gmail.com>
2018-01-05 13:40:37 -0800

    testing deployment for production

--- app/javascript/components/TaskForm.jsx
+++ app/javascript/components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { ROOT_URL } from './TaskList';
+import { ROOT_URL } from './TasksList';
 
 
 class TaskForm extends Component {
